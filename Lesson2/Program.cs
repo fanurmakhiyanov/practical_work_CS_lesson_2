aca3bb04f8712b883395e102b4fdfbc8fc5b5ff7
fanur <makhiyanov.ff@gmail.com>
2022-06-09 22:02:58 +0300

    Array

---
+++ Lesson2/Program.cs
@@ -0,0 +1,30 @@
+﻿void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+int [] array = new int[5];
+int index = array.Length - 1;
+
+while (numberInt > 0)
+{
+    array[index] = numberInt % 10;
+    numberInt = numberInt / 10;
+    index--;
+}
+
+printArray(array);
+
+//while (numberInt > 0)
+//{
+//    array[index] = numberInt % 10;
+//    numberInt = numberInt / 10;
+//    index--;
+//}
