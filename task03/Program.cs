58a2ec965bdb3e1fb2139e3320b1aadcae4044e4
fanur <makhiyanov.ff@gmail.com>
2022-06-12 13:27:35 +0300

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+if (number.Length > 2)
+{
+    Console.WriteLine($"Третьей цифрой введенного числа является " + number[2]);
+}
+else
+Console.Write($"{numberInt} -> Третьей цифры нет");
