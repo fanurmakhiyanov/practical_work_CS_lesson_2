644045de420de4fa89e689d23ebd4ce7e82201e8
fanur <makhiyanov.ff@gmail.com>
2022-06-12 11:43:14 +0300

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное
+// число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 999);
+System.Console.WriteLine(number);
+
+int number1 = number / 100;
+int number2 = number % 100 / 10;
+int number3 = number % 10;
+
+System.Console.Write($"{number1}_{number3}");
